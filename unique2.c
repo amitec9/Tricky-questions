dc4410245e5a56f352446a74b3e91620f52ae334
Amit kumar <eramit3096@gmail.com>
2020-05-25 09:23:57 +0530

    Your branch is up to date with 'origin/master'.

---
+++ unique2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char x =10;
+    short int y = 30000;
+    short int z = 40000;
+    if((x+y)>(x+z))
+    {
+        printf("Good Morning !!");
+    }
+    else
+    {
+        printf("Good Evening! ");
+    }
+    
+
+}
