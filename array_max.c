4f748b3f9c8671dd6d6a5d389959dc1734bb5117
Amit kumar <eramit3096@gmail.com>
2020-05-29 08:19:01 +0530

    Your branch is up to date with 'origin/master'.

--- array_max.c
+++ array_max.c
@@ -2,14 +2,10 @@
 int main()
 {
     int arr[5]={2,3,5,6,1};
-    int max=arr[0];
+    int max=0;
     for(int i=0; i<5;i++)
     {
-        if(max<arr[i])
-        {
-            max=arr[i];
-          
-        }
+       max=max+arr[i]; 
     }
       printf("ARRAY max ===  %d",max);
 }
