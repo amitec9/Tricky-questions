b50c3cd9eaacc8b73b87f93e49cbfc33c079031c
Amit kumar <eramit3096@gmail.com>
2020-06-19 08:07:36 +0530

    Your branch is up to date with 'origin/master'.

---
+++ unique18.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+int main()
+{
+    int x=1,y=20;
+    switch (x)
+    {
+    case 1:
+    ;
+    if(y==20)
+    {
+        case 2:
+            printf("hello");
+    }
+    else
+    {
+        case 3:
+            printf("WORLD");
+    }
+    
+    }
+    return 0;
+    }
+
