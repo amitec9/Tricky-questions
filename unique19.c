b859cf86f27fd066c3e59aa7f86bdf5313818a1d
Amit kumar <eramit3096@gmail.com>
2020-06-20 08:02:52 +0530

    Your branch is up to date with 'origin/master'.

---
+++ unique19.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int i;
+    int *ptr =(int *)malloc(5 * sizeof(int));
+    for(i=0;i<5;i++)
+        *(ptr+ i)=i;
+    printf("%d \n", *ptr++);
+    printf("%d\n", (*ptr)++);
+    printf("%d\n", *ptr);
+    printf("%d\n", *++ptr);
+    printf("%d\n", ++*ptr);
+
+}
