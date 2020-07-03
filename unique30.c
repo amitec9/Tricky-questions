4f3c2af1c8856cb1215e2dfb7818ff298923b19f
Amit kumar <eramit3096@gmail.com>
2020-07-03 09:12:41 +0530

    Your branch is up to date with 'origin/master'.

---
+++ unique30.c
@@ -0,0 +1,17 @@
+//Author :- Amit kumar
+//Date :- 03/07/2020
+#include<stdio.h>
+int main()
+{
+    int n=5;
+    int a=0;
+    while (n)
+    {
+        a=a+n;
+        n--;
+       
+    }
+    printf("%d",a);
+    return 0;
+    
+}
