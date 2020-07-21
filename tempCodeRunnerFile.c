a4aa6e35ace1f01f1202360fd3b85c0946c0db46
Amit kumar <eramit3096@gmail.com>
2020-07-21 07:31:42 +0530

    Your branch is up to date with 'origin/master'.

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,3 @@
+ case 'b'|| 2:
+//         printf("Quiz");
+//        break;
---
+++ unique46.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+   char check = 'a';
+   switch (check)
+   {
+   case 'a'|| 1:
+        printf("coding");
+       break;
+//    case 'b'|| 2:
+//         printf("Quiz");
+//        break;
+   default:
+       printf("codingQuiz");
+   }
+
+}
