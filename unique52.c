b93641101a6733652a59a2f5953f34caa2fe8b91
Amit kumar <eramit3096@gmail.com>
2020-07-27 07:59:51 +0530

    Your branch is up to date with 'origin/master'.

---
+++ unique52.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int c1=0,c2=0;
+    for(int i=0;i<5;i++,c1++)
+        for(int i=0;i<10;i++,c1++)
+
+    for(int i=0;i<5;i++,c2++)
+        for(int i=0;i<10;i++,c2++)
+    printf("%d\n",c1);
+    printf("%d\n",c2);
+}
