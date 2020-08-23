c458d03af565c869204a5147c1ce03aab57f0d88
Amit kumar <49529841+amitec9@users.noreply.github.com>
2020-08-23 18:25:07 +0530

    Add files via upload

---
+++ fun_overriding.cpp
@@ -0,0 +1,29 @@
+//WAP function overriding
+//Author - Amit Kumar
+
+#include<iostream>
+
+using namespace std;
+class Base
+{
+	public:
+	void display()
+	{
+		cout<<"Display of Base" <<endl;
+	}
+};
+
+class Derived:public Base{
+	public:
+	void display()
+	{
+		cout<<"Display of Derived" <<endl;
+	}
+	
+};
+
+int main()
+{
+	Derived d;
+	d.display();
+}
