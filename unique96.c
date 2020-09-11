1fdfceef096aeeece2820f12b575906aadcffaaf
Amit kumar <49529841+amitec9@users.noreply.github.com>
2020-09-11 18:47:27 +0530

    Add files via upload

---
+++ unique96.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int j,sum=0;
+    printf("The first 10 natural number is : ");
+    for(j=1;j <=10;j++)
+    {
+        sum = sum+j;
+        printf("$j");
+    }
+    printf("The sum is : $sum");
+}
