5482430cf189929dd07c244ad67cafd5eb964e84
Amit kumar <49529841+amitec9@users.noreply.github.com>
2020-09-22 08:22:05 +0530

    Add files via upload

---
+++ unique106.c
@@ -0,0 +1,34 @@
+/* C Program to find Sum of Upper Triangle Matrix */
+ 
+#include<stdio.h>
+ 
+int main()
+{
+ 	int i, j, rows, columns, a[10][10], Sum = 0;
+  
+ 	printf("\n Please Enter Number of rows and columns  :  ");
+ 	scanf("%d %d", &i, &j);
+ 
+ 	printf("\n Please Enter the Matrix Elements \n");
+ 	for(rows = 0; rows < i; rows++)
+  	{
+   		for(columns = 0;columns < j;columns++)
+    	{
+      		scanf("%d", &a[rows][columns]);
+    	}
+  	}
+     
+ 	for(rows = 0; rows < i; rows++)
+  	{
+   		for(columns = 0; columns < j; columns++)
+    	{
+    		if(columns > rows)
+    		{
+    			Sum = Sum + a[rows][columns];
+			}
+   	 	}
+  	}
+  	
+  	printf("\n The Sum of Upper Triangle Matrix = %d", Sum);
+ 	return 0;
+}
